188c70a1984b6edddbc9ed8d7b248f4f6f069650
Alicia <xiaojingworld@gmail.com>
2021-07-01 09:04:42 -0400

    Add files via upload

--- 297. Serialize and Deserialize Binary Tree.py
+++ 297. Serialize and Deserialize Binary Tree.py
@@ -1,5 +1,13 @@
 
 import collections
+
+# Definition for a binary tree node.
+class TreeNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
 class Codec:
     def serialize(self, root):
         """Encode a tree to a single string
