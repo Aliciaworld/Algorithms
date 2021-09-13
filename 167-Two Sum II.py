44971fecd72d0001e9a784c7c7fca37df29bed81
Alicia <xiaojingworld@gmail.com>
2021-09-13 09:16:57 -0400

    Update 167-Two Sum II.py

--- 167-Two Sum II.py
+++ 167-Two Sum II.py
@@ -1,5 +1,5 @@
 https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
-
+# 167
 class Solution(object):
     def twoSum(self, numbers, target):
         start = 0
@@ -14,3 +14,5 @@ class Solution(object):
                 start += 1
             else:
                 return [start+1, end+1]
+            
+
