0fe24a37d7a05779be36835ae036da9f17947519
Alicia <xiaojingworld@gmail.com>
2021-10-19 17:22:16 -0400

    Update 136-Single Number.py

--- 136-Single Number.py
+++ 136-Single Number.py
@@ -4,7 +4,6 @@ https://leetcode.com/problems/single-number/
 class Solution(object):
     def singleNumber(self, nums):
         ans = 0
-
         for num in nums:
             ans ^= num
         return ans
